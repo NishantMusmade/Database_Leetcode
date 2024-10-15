c3f0c3959ed03a3a38a5d0b76c051cf0701e521d
NishantMusmade <nishantmusmade.5@gmail.com>
2024-10-15 21:57:33 +0530

    Added

---
+++ employee_earning_more_than_manager.sql
@@ -0,0 +1,16 @@
+-- Write a solution to find the employees who earn more than their managers.
+
+-- Return the result table in any order.
+
+CREATE DATABASE  leetcode;
+
+USE leetcode;
+
+Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int);
+insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3');
+insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4');
+insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', NULL);
+insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', NULL);
+
+SELECT b.name as Employee FROM Employee as a JOIN Employee as b ON a.id = b.managerId WHERE a.salary < b.salary;
+
