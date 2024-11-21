5457b96050393fd54d1999f8ef84cfd6229bcfec
NishantMusmade <nishantmusmade.5@gmail.com>
2024-11-21 07:06:29 +0530

    Updated

---
+++ duplicate_emails.sql
@@ -0,0 +1,9 @@
+USE Leetcode;
+
+Create table If Not Exists Person (id int, email varchar(255));
+
+insert into Person (id, email) values ('1', 'a@b.com');
+insert into Person (id, email) values ('2', 'c@d.com');
+insert into Person (id, email) values ('3', 'a@b.com');
+
+SELECT email FROM Person GROUP BY email HAVING COUNT(email) > 1;
